57899a3d070258d080af9ee7e40e745e4cfe9141
Ayanda Michelle Mumba <ayandamichelle.m@gmail.com>
2024-10-15 14:27:17 +0200

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import ForgotPassword from "./pages/ForgotPassword";
-import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
-import Listings from "./pages/Listings";
-import Dashboard from "./pages/Dashboard";
-import LogIn from "./pages/LogIn";
-import About from "./pages/About";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
+import Listings from "./pages/Listings.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import LogIn from "./pages/LogIn.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
   return(

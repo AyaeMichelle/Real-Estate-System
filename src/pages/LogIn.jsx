10f352a722a6bf4dc78de698a9832f35f6d7cd56
Ayanda Michelle Mumba <ayandamichelle.m@gmail.com>
2024-10-27 10:48:09 +0200

    updated sign-up page functionality

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -24,7 +24,7 @@ export default function LogIn() {
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
-            src="./images/keys.jpg"
+            src="./images/Keys.jpg"
             alt="key"
             className="w-full rounded-2xl"
           />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,25 +1,60 @@
 import { useState } from "react"
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
 import { AiFillEyeInvisible,AiFillEye} from "react-icons/ai";
 
 export default function SignUp() {
   const [showPassword,setShowPassword]= useState(false);
+  const [error,setError]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
   const[formData,SetFormData] =useState({
-    name: "",
+    username: "",
     email: "",
     password: "",
 
   });
-  const{name,email,password} = formData ;
+  const{username,email,password} = formData ;
   function onChange(e){
     SetFormData((prevState)=> ({
       ...prevState,
-      [e.target.id]: e.target.value,
+      [e.target.id]: e.target.value.trim(),
 
     }))
-
-  }
+  };
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    try { 
+    setLoading(true);
+    
+    if(!formData.username || !formData.email  || !formData.password){
+      return setError('Please fill out all fields');
+    }
+      const res= await fetch("http://localhost:3000/api/auth/signup",{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify({
+          username: formData.username, 
+          email: formData.email,
+          password: formData.password,
+        }),
+       });
+       const data=await res.json();
+       console.log(data);
+       if(data.success===false){
+        setLoading(false);
+        setError(data.message);
+        return;
+       }
+       setLoading(false);
+       setError(null);
+       navigate('/log-in');
+      }catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    };
+  
 
   return (
     <section>
@@ -27,17 +62,17 @@ export default function SignUp() {
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img 
-          src ="./images/keys.jpg" alt="key"  
+          src ="./images/Keys.jpg" alt="key"  
           className="w-full rounded-2xl"
           />
           
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={handleSubmit}> 
           <input  
             type ="text" 
-            id="name" 
-            value={name}
+            id="username" 
+            value={username}
              onChange={onChange} 
              placeholder="Full name"
              className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" 
@@ -59,7 +94,8 @@ export default function SignUp() {
              placeholder="Password"
              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" 
              />
-             {showPassword ?( <AiFillEyeInvisible className="absolute right-3 top-3 text-xl cursor-pointer"onClick={()=>setShowPassword((prevState)=>!prevState )} />):(<AiFillEye className="absolute right-3 top-3 text-xl cursor-pointer"onClick={()=>setShowPassword((prevState)=>!prevState )} />) }
+             {showPassword ?( <AiFillEyeInvisible className="absolute right-3 top-3 text-xl cursor-pointer"onClick={()=>setShowPassword((prevState)=>!prevState )} />):(<AiFillEye className="absolute right-3 top-3 text-xl cursor-pointer"
+             onClick={()=>setShowPassword((prevState)=>!prevState )} />) }
              </div>
              <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg ">
               <p className="mb-6"> Have a account?
@@ -69,20 +105,18 @@ export default function SignUp() {
                 <Link to ="/forgot-password"  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out">Forgot Password?</Link>
               </p>
              </div>
-             <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
-              type="submit">Sign up</button>
+             <button disabled={loading} className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              type="submit">{loading ? 'Loading....':'Sign Up'}</button>
                 <div className="my-4  items-center flex before:border-t  before:flex-1 before:border-gray-300  after:border-t  after:flex-1 after:border-gray-300">
                      <p className="text-center font-semibold mx-4">OR</p>
 
                 </div>
-                <OAuth />
-                
-                
+                <OAuth /> 
           </form>
-         
-        
         </div>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </section>
-  )
+  );
 }
+
